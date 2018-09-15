3e7297436738812eac17051d2cebbe150140ae3b
Stef Claus <stef.claus@gmail.com>
2018-09-15 23:43:05 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -9,3 +9,5 @@ def prime?(num)
 end 
 
 sqrt(x)
+
+ A simple but slow method of checking the primality of a given number n, called trial division, tests whether n is a multiple of any integer between 2 and n {\sqrt {n}}} {\sqrt {n}}.
