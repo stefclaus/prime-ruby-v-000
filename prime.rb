cde65b8fbc07946f49c964bc3f6b8f55dd4a1463
Stef Claus <stef.claus@gmail.com>
2018-09-16 00:14:13 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,14 @@
 # Add  code here!
-def prime?(num)
-  if num <= 1 
-    return false 
-  elsif num <= 3
+def prime?(n)
+  if n <= 1
+    return false
+  elsif n <= 3
     return true
-  else (2..num/2).none? do |x|
-    num % x == 0
-    return false 
-  end 
- end    
-end 
-
+  else (2..n/2).none? do |x|
+    n % x == 0
+  end
+  end
+end
 # sqrt(x)
 
 # A simple but slow method of checking the primality of a given number n, called trial division, tests whether n is a multiple of any integer between 2 and n {\sqrt {n}}} {\sqrt {n}}.
